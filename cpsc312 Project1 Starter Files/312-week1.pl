5abe78c5042924f121ddfd921d68dddcae20988a
Derek Chan <derekchan1994@gmail.com>
2015-10-29 13:32:46 -0700

    Finished part 2 to week 1 deliverables

--- cpsc312 Project1 Starter Files/312-week1.pl
+++ cpsc312 Project1 Starter Files/312-week1.pl
@@ -14,3 +14,24 @@
 definition(Word, Meaning) :- 
 	s(Synset_id, _, Word, _, _, _), % Get the Synset_id from s
 	g(Synset_id, Meaning). % Get the Meaning from g (the glossary)
+
+% Part 2:
+% Retrieving morphological phrasings
+%
+% We will require ProNTo_Morph's engine to get all of the 
+% morphological phrasings.
+% 
+% Import ProNTo_Morph with the following:
+% 	[pronto_morph_engine].
+
+word_line_morphs() :- read_word(CharArray), morph_tokens_bag(w(CharArray), Bag), write(Bag).
+
+% read_word is a helper procedure to take an input word
+% from the user and split it into a character list.
+%
+% This code was mostly taken from Steve Wolfman's 312-pess-grammar.pl
+read_word([]) :- peek_char(Ch), Ch = ' ', !.
+read_word([]) :- peek_char(Ch), Ch = '.', !.
+read_word([]) :- peek_char(Ch), Ch = 'end_of_file', !.
+read_word([]) :- peek_char(Ch), Ch = '\n', !.
+read_word([Ch|Chs]) :- get_char(Ch), read_word(Chs).
