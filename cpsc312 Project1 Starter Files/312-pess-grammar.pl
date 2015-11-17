46116da59878fb742687b2cec0cb7eb758292e9b
Derek Chan <derekchan1994@gmail.com>
2015-11-17 12:42:36 -0800

    dictionary lookups against wordnet works
    Cleaned up the vocab expanding procedures
    
    Added the new_X procedures to the NPL clauses
    
    wordnet's 's' type is now also considered as adjectives
    
    Added an ignore list to stop the parsing of determinants. Cleaned up procedure names

--- cpsc312 Project1 Starter Files/312-pess-grammar.pl
+++ cpsc312 Project1 Starter Files/312-pess-grammar.pl
@@ -383,21 +383,23 @@ det_opt_an --> [an].
 n([]) --> [it].                           % "it" is ignored
 n([attr(is_a,X,[])]) --> [X], { n(X) }.   % Anything listed below.
 n([attr(is_a,Name,[])]) --> lit(n, Name). % Any literal tagged as 'n'
-
+n([attr(is_a,X,[])]) --> [X], { new_n(X) }.   % Anything we can add
 
 % Adverbs are either those provided below or literals.
 adv([attr(is_how,X,[])]) --> [X], { adv(X) }.
 adv([attr(is_how,Name,[])]) --> lit(adv, Name).
+adv([attr(is_how,X,[])]) --> [X], { new_adv(X) }.
 
 % Adjectives are either those provided below or literals.
 adj([attr(is_like,X,[])]) --> [X], { adj(X) }.
 adj([attr(is_like,Name,[])]) --> lit(adj, Name).
-
+adj([attr(is_like,X,[])]) --> [X], { new_adj(X) }.
 
 % "Doing" verbs (as opposed to "has" and "is".
 % Either provided below or literals.
 vdoes([attr(does,X,[])]) --> [X], { v(X) }.
 vdoes([attr(does,Name,[])]) --> lit(v, Name).
+vdoes([attr(does,X,[])]) --> [X], { new_v(X) }.
 
 % "Having" verbs are "has" or "have" and "contain" or "contains".
 % The semi-colon is disjunction (just syntactic sugar
@@ -800,3 +802,25 @@ v(scavenges).
 v(quacks).
 v(summers).
 v(winters).
+
+% Procedures for looking up new words from WordNet
+new_n(NewWord) :- \+ignore(NewWord),\+n(NewWord),lookupNewWord(NewWord,n),assertz(n(NewWord)).
+new_adv(NewWord) :- \+ignore(NewWord),\+adv(NewWord),lookupNewWord(NewWord,s),assertz(adv(NewWord)).
+new_adj(NewWord) :- \+ignore(NewWord),\+adj(NewWord),lookupNewWord(NewWord,a),assertz(adj(NewWord)).
+new_adj(NewWord) :- \+ignore(NewWord),\+adj(NewWord),lookupNewWord(NewWord,s),assertz(adj(NewWord)).
+new_v(NewWord) :- \+ignore(NewWord),\+v(NewWord),lookupNewWord(NewWord,v),assertz(v(NewWord)).
+% Import pronto_morph and wordnet
+:- consult('pronto_morph_engine.pl').
+:- consult('wordnet_prolog_2007/wn_s.pl').
+lookupNewWord(Word,Type) :- morph_atoms_bag(Word, Bag),bag_to_stems(Bag,Type). 
+% Grab the stems out of the bag
+bag_to_stems([H|T],Type) :- check_wordnet_definition(H,Type),!.
+bag_to_stems([_|T],Type) :- bag_to_stems(T,Type).
+% Determine the type from WordNet
+check_wordnet_definition([[W|_]],Type) :- s(_,_,W,Type,_,_),!,write('        Added word:'),write(W),write(' type:'),write(Type),nl.
+
+% Ignore list - These are words that get incorrectly interpreted
+ignore(a).
+ignore(an).
+ignore(the).
+ignore(its).
